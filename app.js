89c0bd441d710bb53b1603444e62ee5cd14ec4f2
anil.rai01 <anil.rai01041998@gmail.com>
2019-07-23 19:18:36 +0545

    New Version Working Calc

--- app.js
+++ app.js
@@ -1,103 +1,93 @@
+var numbers = document.querySelectorAll(".btn-num");
+var operators = document.querySelectorAll(".btn-operator");
+var ans = document.querySelector(".btn-operator-ans");
+var result = document.getElementById("result");
+var hist = document.querySelector(".history");
+var c = document.querySelector(".btn-operator-c");
+var ce = document.querySelector(".btn-operator-ce");
 
-var row1 = document.querySelector('.row1');
-var row2 = document.querySelector('.row2');
-var row3 = document.querySelector('.row3');
-var row4 = document.querySelector('.row4');
-var row5 = document.querySelector('.row5');
+var numArray = Array.from(numbers);
+var operArray = Array.from(operators);
 
-row1.addEventListener("click",doSomething, false);
-row2.addEventListener("click",doSomething, false);
-row3.addEventListener("click",doSomething, false);
-row4.addEventListener("click",doSomething, false);
-row5.addEventListener("click",doSomething, false);
+var startCalculation = (function() {
+  var expressionArray = [];
+  let expressString = "";
 
-var finished=false;
+  numArray.forEach(el =>
+    el.addEventListener("click", () => {
+      if (
+        expressionArray[expressionArray.length - 1] === "." &&
+        el.innerHTML === "."
+      ) {
+        return -1;
+      } else {
+        conCat_(el.innerHTML);
+      }
+    })
+  );
 
-function doSomething(e){
-		if (e.target !== e.currentTarget){
-				const key = e.target;
-				const action = key.dataset.action;
-				var expression = document.querySelector('#expression');
-				var result = document.querySelector('#result');
-				var operator = document.querySelector('#operator');
-				if(finished){
-						expression.innerHTML="";
-						finished=false;
-				}
-				if(!action){
-						const num = key.textContent;
-						if(num === '.'){
-								if(result.innerHTML.indexOf(num)>-1){
-										result.innerHTML = result.innerHTML;
-								}
-								else{
-										result.innerHTML = result.innerHTML += num;
-								}
-						}
-						else{
-								result.innerHTML = result.innerHTML += num;
-						}
-				}
-				else if (action == "cancel"){
-						result.innerHTML= result.innerHTML.substring(0, result.innerHTML.length - 1);
-				}
-				else if (action == "clear"){
-						result.innerHTML= "";
-						expression.innerHTML = "";
-						operator.innerHTML = "";
-				}
-				else if (
-						action === 'add' ||
-						action === 'subtract' ||
-						action === 'multiply' ||
-						action === 'divide'
-				) {
-						if(expression.innerHTML == ""){
-								expression.innerHTML = result.innerHTML;
-						}
-						else{
-								expression.innerHTML = operate(expression.innerHTML,result.innerHTML,key.textContent);
-						}
-						result.innerHTML="";
-						let oper = key.textContent;
-						operator.innerHTML=oper;
-				}
-				else if (action === 'equal'){
-						expression.innerHTML = operate(expression.innerHTML,result.innerHTML,operator.innerHTML);
-						result.innerHTML="";
-						operator.innerHTML="";
-						finished=true;
-				}
-		}
-		// e.stopPropagation();
-}
+  operArray.forEach(el =>
+    el.addEventListener("click", () => {
+      if (expressionArray[expressionArray.length - 1] === el.innerHTML) {
+        return -1;
+      } else {
+        conCat_(el.innerHTML);
+      }
+    })
+  );
 
-function operate(operand1, operand2, operator){
-		if(operand1 == ""){
-				alert("Cannot operate on empty operand");
-		}
-		else{
-				operand1 = Number(operand1);
-				operand2 = Number(operand2);
-				var result;
-				if(operator == "+"){
-						result = operand1 + operand2;
-				}
-				else if(operator == "-"){
-						result = operand1 - operand2;
-				}
-				else if(operator == "x"){
-						result = operand1 * operand2;
-				}
-				else if(operator == "/"){
-						result = operand1/operand2;
-				}
-				if(result%1===0){
-						return result;
-				}
-				else{
-						return result.toFixed(2);
-				}
-		}
+  ce.addEventListener("click", () => {
+    clearScreen();
+  });
 
-}
+  var clearScreen = () => {
+    console.log("helo");
+    expressionArray.pop();
+    console.log(expressionArray);
+
+    expressString = expressString.substring(0, expressString.length - 1);
+    display(expressString);
+  };
+
+  var conCat_ = exp => {
+    expressionArray.push(exp);
+    console.log(expressionArray);
+
+    expressString = expressString.concat(exp);
+    display(expressString);
+  };
+
+  function display(el) {
+    // console.log(el);
+    result.innerHTML = el;
+  }
+
+  return {
+    queryArray: expressionArray,
+    // queryExp: expressString,
+    test: 100
+  };
+})(updateAns);
+
+var updateAns = (function(startCalc) {
+  ans.addEventListener("click", () => {
+    var express = "";
+    var exp = startCalc.queryArray;
+
+    exp.forEach(el => {
+      express = express.concat(el);
+    });
+
+    result.innerHTML = "";
+    history.innerHTML = "";
+    var ans = eval(express);
+    hist.innerHTML = express;
+    console.log(express);
+    result.innerHTML = ans;
+  });
+
+  c.addEventListener("click", () => {
+    result.innerHTML = "";
+    hist.innerHTML = "";
+  });
+})(startCalculation);
